20661cc932247edaf3edafba4517281bc454e019
Selvin <46880630+selvinmedina@users.noreply.github.com>
2024-06-08 22:30:42 -0600

    login con validaciones

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'atm-simulador-v2' title`, () => {
+  it(`should have the 'atm-simulador' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('atm-simulador-v2');
+    expect(app.title).toEqual('atm-simulador');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, atm-simulador-v2');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, atm-simulador');
   });
 });
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,16 +2,31 @@ import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     MatInputModule,
     MatButtonModule,
-    MatCardModule],
+    MatCardModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  loginForm = this.fb.nonNullable.group({
+    nombreUsuario: ['', [Validators.required, Validators.maxLength(255)]],
+    pin: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(4)]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Lógica de inicio de sesión
+      console.log('Formulario válido', this.loginForm.value);
+    }
+  }
 }
